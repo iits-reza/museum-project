c0a9040cc08890c0c3017193c7b256b9ede9293c
rezamohdev <mohammadireza29@gmail.com>
2025-01-02 19:45:46 -0500

    add scroll icon

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
-import logo from "./logo.svg";
-import ScrollIcon from "./images/mouseScroll.gif";
+import Lottie from "react-lottie";
+import ScrollIcon from "./lotties/mouse_animation";
 import "./App.css";
 import LotusAnimation from "./components/LotusAnimation";
-import Header from "./components/header/header";
+// import Header from "./components/header/header";
 
 function App() {
+  const defaultOptions = {
+    loop: true,
+    autoplay: true,
+    animationData: ScrollIcon,
+    rendererSettings: {
+      preserveAspectRatio: "xMidYMid slice",
+    },
+  };
   return (
     <div className="App">
       {/* <Header /> */}
-      <img src={ScrollIcon} />
+      <Lottie options={defaultOptions} height={40} width={40} />
       <LotusAnimation />
     </div>
   );
