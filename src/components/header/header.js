6dbda2c449ef9139d46f2adf1a3af9c8d109141d
rezamohdev <mohammadireza29@gmail.com>
2025-01-21 20:22:53 -0500

    header mask effect

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,10 +1,54 @@
+import { useEffect, useState } from "react";
+import { motion } from "framer-motion";
 import "./header.css";
 const Header = () => {
+  const [mousePosition, setMousePosition] = useState({
+    x: 0,
+    y: 0,
+  });
+  const [cursorVariant, setCursorVariant] = useState("default");
+
+  useEffect(() => {
+    const mouseMove = (e) => {
+      setMousePosition({
+        x: e.clientX,
+        y: e.clientY,
+      });
+    };
+
+    window.addEventListener("mousemove", mouseMove);
+
+    return () => {
+      window.removeEventListener("mousemove", mouseMove);
+    };
+  }, []);
+
+  const variants = {
+    default: {
+      x: mousePosition.x - 16,
+      y: mousePosition.y - 16,
+    },
+    text: {
+      height: 150,
+      width: 150,
+      x: mousePosition.x - 75,
+      y: mousePosition.y - 75,
+      backgroundColor: "rgb(50, 126, 248)",
+      mixBlendMode: "difference",
+    },
+  };
+  const textEnter = () => setCursorVariant("text");
+  const textLeave = () => setCursorVariant("default");
   return (
     <header className="header">
-      <h1 className="header__title">Bamiyan wonders</h1>
-      <div className="header__cover"></div>
-      <div className="header__bottom"></div>
+      <h1 onMouseEnter={textEnter} onMouseLeave={textLeave} className="title">
+        Bamiyan The land of wonders
+      </h1>
+      <motion.div
+        className="cursor"
+        variants={variants}
+        animate={cursorVariant}
+      />
     </header>
   );
 };
