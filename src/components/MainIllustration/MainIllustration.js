221b07de78e136524538a45696e47f396e1e1166
rezamohdev <mohammadireza29@gmail.com>
2025-02-06 21:04:52 -0500

    adding more elements and animatins

--- src/components/MainIllustration/MainIllustration.js
+++ src/components/MainIllustration/MainIllustration.js
@@ -1,6 +1,10 @@
 import * as motion from "motion/react-client";
 import "./MainIllustration.css";
 import BuddhaStatueSrc from "../../images/asset_main/buddha_statue.png";
+import FirstBirdSrc from "../../images/asset_main/bird1.png";
+import SecondtBirdSrc from "../../images/asset_main/bird2.png";
+import MountainsSrc from "../../images/asset_main/mountains.png";
+import CloudsSrc from "../../images/asset_main/clouds.png";
 
 import Lottie from "react-lottie";
 import ScrollIcon from "../../lotties/mouse_animation";
@@ -16,9 +20,11 @@ export default function MainIllustration() {
   };
   return (
     <div className="illustration">
-      <div className="illustration_left">
-        <motion.h1>The greate Buddha statue of Bamiyan</motion.h1>
-        <motion.p>
+      <div className="illustration__left">
+        <motion.h1 className="illustration__title">
+          The greate Buddha statues of Bamiyan
+        </motion.h1>
+        <motion.p className="illustration__description">
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
           eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
           minim veniam, quis nostrud exercitation ullamco laboris nisi ut
@@ -28,18 +34,50 @@ export default function MainIllustration() {
           culpa qui officia deserunt mollit anim id est laborum.
         </motion.p>
       </div>
-      <div className="illustration_right">
+      <div className="illustration__right">
         <motion.div
-          className="illustration_bluebg"
+          className="illustration__bluebg"
           initial={{ opacity: 0 }}
           whileInView={{ opacity: 1 }}
         ></motion.div>
         <motion.img
           src={BuddhaStatueSrc}
-          className="illsutration_buddhaImageStyle"
+          className="illsutration__buddhaImageStyle"
           alt="Buddha statue"
           initial={{ translateY: 100 }}
           whileInView={{ translateY: 0 }}
+          transition={{ duration: 1.3 }}
+        />
+        <motion.img
+          src={FirstBirdSrc}
+          className="illsutration__Bird1ImageStyle"
+          alt="Flying bird"
+          initial={{ translateX: -100 }}
+          whileInView={{ translateX: 0 }}
+          transition={{ duration: 1.3 }}
+        />
+        <motion.img
+          src={SecondtBirdSrc}
+          className="illsutration__Bird2ImageStyle"
+          alt="Flying bird"
+          initial={{ translateY: -100 }}
+          whileInView={{ translateY: 0 }}
+          transition={{ duration: 1.3 }}
+        />
+        <motion.img
+          src={MountainsSrc}
+          className="illsutration__mounts"
+          alt="Flying bird"
+          initial={{ translateY: 100 }}
+          whileInView={{ translateY: 0 }}
+          transition={{ duration: 1 }}
+        />
+        <motion.img
+          src={CloudsSrc}
+          className="illsutration__clouds"
+          alt="Flying bird"
+          initial={{ translateX: 100 }}
+          whileInView={{ translateX: 0 }}
           transition={{ duration: 1.3 }}
         />
       </div>
